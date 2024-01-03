3a0554175e372d550e59c74923a6fc26ba171b67
appengine8848 <horizon1231@hotmail.com>
2024-01-03 23:04:37 +0800

    Update teles.py

--- teles.py
+++ teles.py
@@ -1,6 +1,3 @@
-%YAML 1.1
----
-proxies:
   - {name: cf-40065-Chun, server: 103.20.223.171, port: 37811, client-fingerprint: chrome, type: vmess, uuid: 71d0c102-7a03-418b-c8e0-2f6d77f9bf3c, alterId: 0, cipher: auto, tls: true, tfo: false, skip-cert-verify: false, network: ws, ws-opts: {path: "/?ed=2048", headers: {Host: vmess.horizon1231.pp.ua}}, udp: true}
   - {name: cf-40065-Seoul, server: 103.20.223.171, port: 37811, client-fingerprint: chrome, type: vmess, uuid: 71d0c102-7a03-418b-c8e0-2f6d77f9bf3c, alterId: 0, cipher: auto, tls: true, tfo: false, skip-cert-verify: false, network: ws, ws-opts: {path: "/?ed=2048", headers: {Host: vmess2.horizon1231.pp.ua}}, udp: true}
   - {name: cf-jinx, server: 199.15.76.35, port: 443, client-fingerprint: chrome, type: vless, uuid: db6f742c-9da2-4525-b986-fe28e8df2fb2, tls: true, tfo: false, servername: alibabacn2.horizon1231.pp.ua, skip-cert-verify: false, network: ws, ws-opts: {path: "/?ed=2048", headers: {Host: alibabacn2.horizon1231.pp.ua}}, udp: true}
